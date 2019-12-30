d6c9c580447152d3365702ea0d73105ec6ccb51e
michael-bailey <mickyb18a@gmail.com>
2019-12-30 15:41:04 +0000

    added more unit tests to datamanager.

--- src/test/java/client/managers/DataManagerTest.java
+++ src/test/java/client/managers/DataManagerTest.java
@@ -3,7 +3,6 @@ package client.managers;
 import client.ui.*;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -30,9 +29,52 @@ public class DataManagerTest {
         assertTrue(b);
     }
 
-
-    public void testUnlockingOfFile() {
-
+    @Test
+    public void testUnlockingFileCorrectPassword() {
+        new File("helloWorld.dat").delete();
+        DataManager a = new DataManager();
+        a.createNew("helloWorld", "Password1234");
+        a = null;
+        a = new DataManager();
+        boolean b = a.unlock("helloWorld", "Password1234");
+        assertTrue(b);
     }
+
+    @Test
+    public void testUnlockingFileWrongPassword() {
+        new File("helloWorld.dat").delete();
+        DataManager a = new DataManager();
+        a.createNew("helloWorld", "Password1234");
+        a = null;
+        a = new DataManager();
+        boolean b = a.unlock("helloWorld", "password1234");
+        assertTrue(!b);
+    }
+
+    @Test
+    public void testUnlockingFileWrongName() {
+        new File("helloWorld.dat").delete();
+        DataManager a = new DataManager();
+        a.createNew("helloWorld", "Password1234");
+        a = null;
+        a = new DataManager();
+        boolean b = a.unlock("helloworld", "Password1234");
+        assertTrue(!b);
+    }
+
+    @Test
+    public void testAddingValues() {
+        
+    }
+
+/*
+    public void testLockingOfFile() {
+        new File("helloWorld.dat").delete();
+        DataManager a = new DataManager();
+        a.createNew("helloWorld", "Password1234");
+        boolean b = a.lock();
+        assertTrue(!b);
+    }
+*/
     
 }
