46eb187ece97496c71c9f99086ac46398074b665
michael bailey <mickyb18a@gmail.com>
2020-02-15 23:28:39 +0000

    started work on the contact pane

--- src/main/java/client/ui/main_window/contact_pane/ContactPane.java
+++ src/main/java/client/ui/main_window/contact_pane/ContactPane.java
@@ -1,6 +1,20 @@
 package client.ui.main_window.contact_pane;
 
+import javafx.collections.FXCollections;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.ArrayList;
+
 public class ContactPane extends AnchorPane {
+
+    private ListView<ContactBox> listView = new ListView<>();
+
+    public ContactPane() {
+
+    }
+
+    void setContacts(ArrayList contacts) {
+        this.listView.setItems(FXCollections.observableList(contacts));
+    }
 }
