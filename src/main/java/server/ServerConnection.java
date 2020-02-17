60c7275bc4ac29b082458329041be553ecc5ce69
Mitchell <mitchellhardie1@gmail.com>
2020-02-17 07:49:24 +0000

    Fixed server to accept multiple hosts
    added caching of connected clients so anyone can be pinged when needed.

--- src/main/java/server/ServerConnection.java
+++ src/main/java/server/ServerConnection.java
@@ -1,14 +1,20 @@
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.BufferedReader;
+import java.io.PrintWriter;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.net.SocketAddress;
 
 public class ServerConnection{
 	private final int PORT = 9806;
 	private boolean doStop = false;
 	private ServerSocket ss;
+	private ArrayList<Socket> connectedUsers = new ArrayList<Socket>();
 
-	public ServerConnection(){
+	public ServerConnection(){}
+
+	public void startServer(){
 		try{
 			this.ss = new ServerSocket(PORT);
 			System.out.println("Listening for Connections...");
@@ -17,6 +23,7 @@ public class ServerConnection{
 				System.out.println("Connection Established");
 				System.out.println("Creating new client thread...");
 				createClientThread(socket);
+				connectedUsers.add(socket);
 			}
 		}catch(Exception e){
 			System.out.println("ServerConnection Constructor error!");
@@ -56,14 +63,29 @@ public class ServerConnection{
 		@Override
 		public void run(){
 			try{
-				System.out.println("Client Thread Created");
-				BufferedReader dataInput = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-				String str = dataInput.readLine();
-				System.out.println(str);
+				while(!this.doStop){
+					System.out.println("Client Thread Created");
+					BufferedReader dataInput = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+					String str = dataInput.readLine();
+					System.out.println(str);
+					if(str=="quit"){
+						this.doStop();
+					}else{
+						for(int i=0;i<connectedUsers.size();i++){
+							if(this.socket!=connectedUsers.get(i)){
+								PrintWriter pr = new PrintWriter(connectedUsers.get(i).getOutputStream());
+								pr.println(str);
+								pr.flush();
+							}
+						}
+					}
+				}
 				this.socket.close();
 			}catch(Exception e){
 				System.out.println("ClientThread Error!");
-				this.socket.close();
+				try{
+					this.socket.close();
+				}catch(Exception ex){System.out.println("Connection could not be closed!");}
 			}
 		}
 	}
