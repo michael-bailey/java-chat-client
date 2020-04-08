43ed4edd010c1c89995ddfcbdaa69b3f04a8f886
Mitchell <mitchellhardie1@gmail.com>
2020-04-08 23:31:10 +0000

    moved to server to its own repo

--- src/main/rust/server-rust/target/debug/server-rust.d
+++
@@ -1,1 +0,0 @@
-/home/mitchell/Documents/programming/java/java-chat-client/src/main/rust/server-rust/target/debug/server-rust: /home/mitchell/Documents/programming/java/java-chat-client/src/main/rust/server-rust/src/main.rs
