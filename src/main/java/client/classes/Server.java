1d78d4a8f793bc5ef63febbf42e9c9ff933a769a
michael bailey <mickyb18a@gmail.com>
2020-05-17 17:00:21 +0100

    added server builder

--- src/main/java/client/classes/Server.java
+++ src/main/java/client/classes/Server.java
@@ -6,23 +6,57 @@ import java.net.InetAddress;
 import java.util.UUID;
 
 public class Server implements Serializable {
+
+
     private static final long serialVersionUID = 7209367093717893717L;
 
-    UUID uuid;
-    String host;
-    String serverName;
+    private final String host;
+    private final String name;
+    private final String owner;
 
-    public Server(String host, String serverName, UUID uuid) {
-        this.host = host;
-        this.serverName = serverName;
-        this.uuid = uuid;
+
+    public Server(Builder builder) {
+        this.host = builder.host;
+        this.name = builder.name;
+        this.owner = builder.owner;
+    }
+
+    public static class Builder {
+        String owner;
+        String host;
+        String name;
+
+        public Builder host(String host) {
+            this.host = host;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder owner(String owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public Server build() {
+            return new Server( this);
+        }
+
+
     }
 
     public String getIpAddress() {
         return host;
     }
 
-    public String getServerName() {
-        return serverName;
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
     }
 }
