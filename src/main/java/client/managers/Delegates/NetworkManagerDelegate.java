c3f25bc8e9dfaaf9d77aba958291bb16d073f3e5
michael bailey <mickyb18a@gmail.com>
2020-05-20 21:21:38 +0100

    Update NetworkManagerDelegate.java
    ~ added some default parameters tot the default network manager delegate.

--- src/main/java/client/managers/Delegates/NetworkManagerDelegate.java
+++ src/main/java/client/managers/Delegates/NetworkManagerDelegate.java
@@ -1,5 +1,21 @@
 package client.managers.Delegates;
 
+import client.classes.Contact;
+
 public class NetworkManagerDelegate implements INetworkManagerDelegate {
 
+    @Override
+    public void ptpReceivedMessage() {
+        System.out.println("received message ptp");
+    }
+
+    @Override
+    public void stdReceivedMessage() {
+        System.out.println("received message server");
+    }
+
+    @Override
+    public Contact[] updateClientList() {
+        return new Contact[0];
+    }
 }
