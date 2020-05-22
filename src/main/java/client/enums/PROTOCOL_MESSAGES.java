b6d025f640a2b3dcd139acff71fefe5f9e33c5e7
michael bailey <mickyb18a@gmail.com>
2020-05-22 22:54:57 +0100

    Create PROTOCOL_MESSAGES.java
    + created a class to hold constants for each type of message.

---
+++ src/main/java/client/enums/PROTOCOL_MESSAGES.java
@@ -0,0 +1,13 @@
+package client.enums;
+
+public class PROTOCOL_MESSAGES {
+    public final static String REQUEST = "?request:";
+    public final static String SUCCESS = "!success:";
+    public final static String INFO = "?info!";
+    public final static String TEST = "!test:";
+    public final static String MESSAGE = "!message:";
+    public final static String CONNECT = "!connect:";
+    public final static String UPDATE_CLIENTS = "!clientUpdate:";
+    public final static String CLIENT = "!client:";
+    public final static String DISCONNECT = "!disconnect:";
+}
