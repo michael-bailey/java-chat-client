bf3846690f4601d13c5b4af750792a31393d4a8a
michael bailey <mickyb18a@gmail.com>
2020-06-07 19:54:58 +0100

    Implemeting the connect test

--- src/test/java/client/managers/NetworkModules/ServerModuleTest.java
+++ src/test/java/client/managers/NetworkModules/ServerModuleTest.java
@@ -1,6 +1,8 @@
 package client.managers.NetworkModules;
 
+import client.Delegates.Interfaces.IServerModuleDelegate;
 import client.StorageDataTypes.Account;
+import client.StorageDataTypes.Contact;
 import client.StorageDataTypes.Server;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -9,10 +11,10 @@ import server.classes.JavaServer;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.*;
 
-public class ServerModuleTest {
+public class ServerModuleTest implements IServerModuleDelegate {
 
     private static Account account;
     private static JavaServer server;
@@ -33,12 +35,20 @@ public class ServerModuleTest {
     }
 
     @Test
-    // todo fix this test somehow
     public void testGetServerDetails() {
-        Server serverDetails;
-        serverDetails = serverModule.getServerDetails("127.0.0.1");
+        Server serverDetails = serverModule.getServerDetails("127.0.0.1");
+        assertEquals("127.0.0.1", serverDetails.host);
         assertEquals("testserver", serverDetails.name);
         assertEquals("michael-bailey", serverDetails.owner);
+    }
+
+    @Test
+    public void testConnectReceiveClientsAndDisconnect() throws InterruptedException {
+        var serverDetails = serverModule.getServerDetails("127.0.0.1");
+        serverModule.connect(serverDetails);
+        serverModule = new ServerModule(this);
+        sleep(1000);
+        serverModule.disconnect();
     }
 
     @AfterClass
@@ -47,5 +57,27 @@ public class ServerModuleTest {
         server = null;
         serverModule = null;
         account = null;
+    }
+
+    // MARK: delegate methods
+
+    @Override
+    public void serverWillConnect() {
+        System.out.println("server is attempting a connection");
+    }
+
+    @Override
+    public void serverDidConnect() {
+        System.out.println("server has connected");
+    }
+
+    @Override
+    public void serverWillUpdateClients() {
+        System.out.println("updating clients");
+    }
+
+    @Override
+    public void serverDidUpdateClients(Contact[] clients) {
+        assertNotNull(clients);
     }
 }
