ee1e451719d466b0587add0295e0ecc5c96826af
michael-bailey <mickyb18a@gmail.com>
2020-07-02 20:52:29 +0100

    Update DataStore.java

--- src/main/java/io/github/michael_bailey/client/managers/DataStore/DataStore.java
+++ src/main/java/io/github/michael_bailey/client/managers/DataStore/DataStore.java
@@ -2,6 +2,7 @@ package io.github.michael_bailey.client.managers.DataStore;
 
 import java.io.Serializable;
 
+@Deprecated
 public class DataStore implements Serializable {
 
     private static final long serialVersionUID = -5813387299140430418L;
