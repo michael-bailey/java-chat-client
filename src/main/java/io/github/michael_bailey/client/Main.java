b4f59319cdcedd7d8ed2968ca57ab65f9e72ed5a
michael-bailey <mickyb18a@gmail.com>
2020-09-26 16:13:58 +0100

    Update Main.java

--- src/main/java/io/github/michael_bailey/client/Main.java
+++ src/main/java/io/github/michael_bailey/client/Main.java
@@ -2,7 +2,6 @@ package io.github.michael_bailey.client;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-    	int a = 1
         ProgramController.main(args);
     }
 }
